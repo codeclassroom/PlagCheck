b60145dcfcb77f90e0c54147fe7a511bd44a154c
Bhupesh-V <varshneybhupesh@gmail.com>
2019-09-22 17:56:00 +0530

    function for scraping the results

--- check.py
+++ check.py
@@ -1,12 +1,14 @@
 import mosspy
 import os
+import requests
+from bs4 import BeautifulSoup as bs
 from dotenv import load_dotenv
 load_dotenv()
 
 userid = os.environ['USER_ID']
 
 # Get Moss Results Report URL
-def detectCheat(program_list, language):
+def submitFiles(program_list, language):
 	m = mosspy.Moss(userid, language)
 	for item in program_list:
 		m.addFile(item)
@@ -16,3 +18,18 @@ def detectCheat(program_list, language):
 # Download whole report locally including code diff links
 def downloadReport(url):
 	mosspy.download_report(url, "submission/report/", connections=8)
+
+# Scrape the webpage for percentage match etc.
+def extractInfo(url):
+	res = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+	html = bs(res.text, "lxml")
+
+	for row in html.find_all('tr'):
+		cols = row.find_all('td')
+		cols = [x.text.strip() for x in cols]
+		print(cols)
+
+
+url = submitFiles(['test_python.py', 'test_python2.py'], "Python")
+print(url)
+extractInfo(url)
