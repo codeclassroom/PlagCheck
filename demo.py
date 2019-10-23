3bb248b4a20eba81973887594fb0398c1915657a
Alexey Dubrov <vhsw@ya.ru>
2019-10-23 13:12:34 +0300

    test, travis

--- demo.py
+++ demo.py
@@ -1,16 +1,14 @@
+"""Usage example"""
 import os
-import PlagCheck.check as plagcheck
-from dotenv import load_dotenv
-
-load_dotenv()
-
-userid = os.environ["USER_ID"]
+import pprint
+from plagcheck import plagcheck
 
 program_files = ["testfiles/test_python.py", "testfiles/test_python3.py"]
-language = "Python"
+language = "python"
+userid = os.environ["USER_ID"]
 
 url, results = plagcheck.check(program_files, language, userid)
 
 
 print(url)
-print(results)
+pprint.pprint(results)
